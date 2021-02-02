12a1ff479ed6867914f6285094a9b18f3b63b443
Kim Sandberg <s@ndberg.dk>
2021-02-02 14:08:02 +0100

    Filter generated in savefile.

---
+++ InstallDetection/FilterElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonistoRepackage.InstallDetection
+{
+    class FilterElement
+    {
+        string drive { get; set; }
+        string path { get; set; }
+        string file { get; set; }
+        string fileType { get; set; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security.Principal;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,9 +27,11 @@ namespace SonistoRepackage
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-       
+
         [NotifyPropertyChangedInvocator]
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
@@ -41,10 +45,13 @@ namespace SonistoRepackage
         int totalNumberOfActivities = 0;
         int numberOfEntriesInList = 0;
 
+
+        Dictionary<string, FilterElement> filterElements = new Dictionary<string, FilterElement>();
+
         public MainWindow()
         {
             InitializeComponent();
-            rtbInfoWindow.Document.Blocks.Add(new Paragraph(new Run("test")));
+            //rtbInfoWindow.Document.Blocks.Add(new Paragraph(new Run("test")));
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
@@ -71,6 +78,13 @@ namespace SonistoRepackage
 
                     //Start watching
                     watcher.EnableRaisingEvents = true;
+
+                    while(eventList.Count < filterElements.Count)
+                    {
+
+                    }
+
+
                 }
             }
 
@@ -101,9 +115,10 @@ namespace SonistoRepackage
                     lock (synchLock)
                     {
                         rtbInfoWindow.Document.Blocks.Add(new Paragraph(new Run(text)));
+                        /*rtbInfoWindow.Document.Blocks.Add(new Paragraph(new Run(text)));
                         rtbInfoWindow.Focus();
                         rtbInfoWindow.ScrollToEnd();
-                        rtbInfoWindow.UpdateLayout();
+                        rtbInfoWindow.UpdateLayout();*/
                     }
 
                     //OnPropertyChanged("textLine");
@@ -133,10 +148,11 @@ namespace SonistoRepackage
 
                     text = " |File:" + e.FullPath + " |Action:" + e.ChangeType + " |Owner:" + owner + "\n";
                     var paragraph = new Paragraph();
-                    paragraph.Inlines.Add(new Run(text));
-                    rtbInfoWindow.Document.Blocks.Add(paragraph);
+                    //paragraph.Inlines.Add(new Run(text));
+                    rtbInfoWindow.Document.Blocks.Add(new Paragraph(new Run(text)));
+                   /* rtbInfoWindow.Document.Blocks.Add(paragraph);
                     rtbInfoWindow.Focus();
-                    rtbInfoWindow.ScrollToEnd();
+                    rtbInfoWindow.ScrollToEnd();*/
                     //OnPropertyChanged("textLine");
                     //eventList.Add(" |File:" + e.FullPath + " |Action:" + e.ChangeType + " |Owner:" + owner);
                     totalNumberOfActivities += 1;
@@ -156,6 +172,93 @@ namespace SonistoRepackage
         {
 
         }
+
+        private void btnCreateFilter_Click(object sender, RoutedEventArgs e)
+        {
+            // run innounp.exe -v installerfile -> filter.txt
+            executeInnounp(this.txtBxPath.Text, this.txtBxInstaller.Text);
+            // Read filter.txt, and put the filenames into a Dictionary
+
+        }
+
+        private void executeInnounp(string path, string filename)
+        {
+            {
+
+                // Use ProcessStartInfo class
+                ProcessStartInfo innounpProces = new ProcessStartInfo();
+                innounpProces.CreateNoWindow = false;
+                innounpProces.UseShellExecute = false;
+                innounpProces.FileName = "innounp.exe";
+                innounpProces.WorkingDirectory = path;
+                innounpProces.WindowStyle = ProcessWindowStyle.Normal;
+                innounpProces.RedirectStandardOutput = true;
+
+                string arguments = "-v \"" + filename + "\"";
+                
+                innounpProces.Arguments = arguments;                
+                
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(innounpProces))
+
+                    {
+                        string line = "";
+                        while (!exeProcess.StandardOutput.EndOfStream)
+                        {
+                            line = line + exeProcess.StandardOutput.ReadLine() + "\n";
+                        }
+                        exeProcess.WaitForExit();
+                        int idx = filename.IndexOf(".");
+                        string filterFileName = filename.Substring(0, idx) + "_filter.txt";
+                        string filterFile = path + filterFileName;
+                        if (File.Exists(path + filterFileName))
+                        {
+                            File.Delete(path + filterFileName);
+                        }
+                        File.WriteAllText(path + filterFileName, line);
+                        //int exitCode = exeProcess.ExitCode;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        private void btnFindInstaller_Click(object sender, RoutedEventArgs e)
+        {
+            // https://www.c-sharpcorner.com/UploadFile/mahesh/openfiledialog-in-wpf/
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+            openFileDlg.DefaultExt = ".exe";
+            openFileDlg.Filter = "Inno executable (.exe)| *.exe";
+            openFileDlg.InitialDirectory = @"C:\Temp\";
+
+            // Launch OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+            if (result == true)
+            {
+                string file = openFileDlg.FileName;
+                int to = file.Length - 1;
+                int lastOccurance = file.LastIndexOf(@"\");
+                string filename = file.Substring(lastOccurance+1, to-lastOccurance);
+                string path = file.Replace(filename, "");
+                this.txtBxPath.Text = path;
+                this.txtBxInstaller.Text = filename;
+            }
+        }
+
+        private void btnSelectJsonPath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
 
         /*protected void OnPropertyChanged(string propertyName)
         {
