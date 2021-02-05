1aec03826f41fc380cb718f6be3d8a92306ddc06
Kim Sandberg <s@ndberg.dk>
2021-02-05 08:22:34 +0100

    New approach. Got handle64.exe from microsoft.
    See if this util can produce the output that we need. it is keeping an eye on a named process to see which handles it has open.

--- InstallDetection/Detection.cs
+++ InstallDetection/Detection.cs
@@ -6,6 +6,7 @@ using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Diagnostics;
 
 namespace SonistoRepackage.InstallDetection
 {
@@ -13,16 +14,87 @@ namespace SonistoRepackage.InstallDetection
     {
         bool startRecord = false;
         List<String> eventList = new List<string>();
+        string processName = "";
+        string workingDirectory = "";
+        string filesData = "";
 
 
         Dictionary<int, WatchedElement> watchedList = new Dictionary<int, WatchedElement>();
 
-        public Detection()
+        public Detection(string processName, string workingDirectory, string filesData)
         {
-            
+            this.processName = processName;
+            this.workingDirectory = workingDirectory;
+            this.filesData = filesData;
         }
 
         public void InstanceMethod()
+        {
+            handleSolution(processName, workingDirectory, filesData);
+            //fileSystemWatcherSolution();
+
+        }
+
+        private void handleSolution(string processName, string workingDirectory, string filesData)
+        {
+            // Use ProcessStartInfo class
+            ProcessStartInfo innounpProces = new ProcessStartInfo();
+            innounpProces.CreateNoWindow = false;
+            innounpProces.UseShellExecute = false;
+            innounpProces.FileName = "handle64.exe";
+            innounpProces.WorkingDirectory = workingDirectory;
+            innounpProces.WindowStyle = ProcessWindowStyle.Normal;
+            innounpProces.RedirectStandardOutput = true;
+            innounpProces.Arguments = "-p \"" + processName + "\"";
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(innounpProces))
+                {
+                    string line = "";
+                    string tmpLine;
+                    int counter = 0;
+                    while (!exeProcess.StandardOutput.EndOfStream)
+                    {
+                        tmpLine = exeProcess.StandardOutput.ReadLine();
+                        //skip the first 3 lines of the datastream
+                        if (counter > 2)
+                        {
+                            if (tmpLine.Contains(":"))
+                            {
+                                eventList.Add(tmpLine);
+                                line = line + tmpLine + "\n";
+                            }
+                        }
+                        counter += 1;
+                    }
+                    exeProcess.WaitForExit();
+
+                    //Getting the filename, path
+
+                    //Creating filename of filter file
+                    int idx = filesData.IndexOf(".");
+                    string filterFileName = filesData.Substring(0, idx) + "_filter.txt";
+                    string filterFile = workingDirectory + filterFileName;
+
+                    //if file exists delete. Create filter file using data from line
+                    if (File.Exists(workingDirectory + filterFileName))
+                    {
+                        File.Delete(workingDirectory + filterFileName);
+                    }
+                    File.WriteAllText(workingDirectory + filterFileName, line);
+                    //int exitCode = exeProcess.ExitCode;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void fileSystemWatcherSolution()
         {
             startRecord = true;
             var drives2 = DriveInfo.GetDrives();
@@ -70,7 +142,7 @@ namespace SonistoRepackage.InstallDetection
 
            
             //string user = Environment.UserName;
-            // WindowsPrincipal myPrincipal = (WindowsPrincipal)Thread.CurrentPrincipal;
+
             WatchedElement watchedElement = new WatchedElement();
             //Test
             string owner = "";
